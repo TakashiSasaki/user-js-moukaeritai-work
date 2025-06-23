39c0ca5d222afd942073b57b809799c9044402b5
Takashi SASAKI <takashi316@gmail.com>
2025-06-23 23:03:40 +0000

    バージョン番号を0.9.5から0.9.6に更新し、ボタンセレクタを修正

--- chatgpt-auto-prompt-sender/chatgpt-auto-prompt-sender.user.js
+++ chatgpt-auto-prompt-sender/chatgpt-auto-prompt-sender.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT Auto Prompt Sender
 // @namespace    https://userscript.moukaeritai.work/
-// @version      0.9.5.20230826
+// @version      0.9.6.20230829
 // @description  Automates sending of next pre-filled prompt in ChatGPT after current response completion.
 // @author       Takashi SASAKI (https://twitter.com/TakashiSasaki)
 // @match        https://chat.openai.com/c/*
@@ -40,8 +40,9 @@ setTimeout(function() {
         const selector20230816 = "#__next > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div.relative.flex.h-full.max-w-full.flex-1.overflow-hidden > div > main > div > div.absolute > form > div > div.flex.flex-col.w-full.flex-grow > button";
         const selector20230823 = "#__next > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div.relative.flex.h-full.max-w-full.flex-1.overflow-hidden > div > main > div > div.absolute > form > div > div.flex > div > button";
         const selector20230826 = "#__next > div > div > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div.relative.flex.h-full.max-w-full.flex-1.overflow-hidden > div > main > div > div.absolute > form > div > div.flex > div > button";
+        const selector20230829 = "#__next > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div.relative.flex.h-full.max-w-full.flex-1.overflow-hidden > div > main > div > div.absolute > form > div > div.flex > div > button";
 
-        const button = document.querySelector(selector20230826);
+        const button = document.querySelector(selector20230829);
         if(!button) {
             alert("The submit button could not be found. The structure of the ChatGPT webpage seems to have changed. Please wait for updates to the ChatGPT Auto Prompt Sender.");
         }
@@ -51,16 +52,6 @@ setTimeout(function() {
                                   subtree: true
                                  });
     });
-
-    /*
-    GM_registerMenuCommand("Open this user script in Github Gist", () => {
-        GM_openInTab('https://gist.github.com/TakashiSasaki/730f930806ec1a6460ab350f7498d622/', { active: true });
-    });
-
-    GM_registerMenuCommand("Open this user script in Greasy Fork", () => {
-        GM_openInTab('https://greasyfork.org/ja/scripts/472713', { active: true });
-    });
-    */
 
     // Your code here...
 }, 1000);
