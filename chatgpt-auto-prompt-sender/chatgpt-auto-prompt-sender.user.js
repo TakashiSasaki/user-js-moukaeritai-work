12b4ee4fa752df863f8fb61e98320e54a4b196e6
Takashi SASAKI <takashi316@gmail.com>
2025-06-23 23:02:43 +0000

    バージョン番号を0.9.3から0.9.4に更新し、ボタンセレクタを修正

--- chatgpt-auto-prompt-sender/chatgpt-auto-prompt-sender.user.js
+++ chatgpt-auto-prompt-sender/chatgpt-auto-prompt-sender.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT Auto Prompt Sender
 // @namespace    https://userscript.moukaeritai.work/
-// @version      0.9.3.20230818
+// @version      0.9.4.20230823
 // @description  Automates sending of next pre-filled prompt in ChatGPT after current response completion.
 // @author       Takashi SASAKI (https://twitter.com/TakashiSasaki)
 // @match        https://chat.openai.com/c/*
@@ -38,8 +38,9 @@ setTimeout(function() {
     GM_registerMenuCommand("Schedule Next Prompt After Ongoing Response", ()=>{
         const selector20230809 = "#__next > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div.relative.flex.h-full.max-w-full.flex-1.overflow-hidden > div > main > div > form > div > div > button";
         const selector20230816 = "#__next > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div.relative.flex.h-full.max-w-full.flex-1.overflow-hidden > div > main > div > div.absolute > form > div > div.flex.flex-col.w-full.flex-grow > button";
+        const selector20230823 = "#__next > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div.relative.flex.h-full.max-w-full.flex-1.overflow-hidden > div > main > div > div.absolute > form > div > div.flex > div > button";
 
-        const button = document.querySelector(selector20230816);
+        const button = document.querySelector(selector20230823);
         button.style.background = "red";
         observer.observe(button, {attributes: true,
                                   childList: true,
